18d8b8d158a55f917e73c58e01b561f5982bd2c4
Helix <hellonike0@gmail.com>
2020-05-18 18:14:19 +0530

    feat (init): filter_text added in python

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ import json
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+def filter_text(str):
+    return str.split()
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return "Hello world"
