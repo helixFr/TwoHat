b94f2be897523ca2a5f89a27eed5afefa56fbd18
Helix <hellonike0@gmail.com>
2020-05-18 18:24:36 +0530

    feat (init): get_topics; store topic maps of filtered words

--- server.py
+++ server.py
@@ -5,14 +5,24 @@ import json
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+def get_topics(word, dicts):
+    word_dict = json.loads(backend.returnFromJson(jsonLoc, word))
+    if word_dict not in dicts:
+        dicts.append(word_dict)
+
 def filter_text(str):
     return str.split()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    dicts = []
     requestString = flask.request.data.decode("utf-8")
     requestDict = json.loads(requestString)
     word_list = filter_text(requestDict["text"])
+    word_list = list(itertools.chain.from_iterable(itertools.repeat(x, 100) for x in word_list))
+    for word in word_list:
+        get_topics(word, dicts)
+    print(dicts)
     return json.dumps(requestDict)
 
 jsonLoc = backend.loadJson()
