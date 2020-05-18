94eb40b1df0403963ceb368a616b53bf463670f7
Helix <hellonike0@gmail.com>
2020-05-18 18:29:45 +0530

    feat (init): merge_topics; select maximum value for each topic; return request json

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import flask
 import backend
 import json
+import itertools
+import time
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -10,11 +12,23 @@ def get_topics(word, dicts):
     if word_dict not in dicts:
         dicts.append(word_dict)
 
+def merge_topics(dicts, merged):
+    for word_dict in dicts:
+        for topic in word_dict.keys():
+            res = -1
+            try:
+                res = merged[topic]
+            except:
+                pass
+            if word_dict[topic] > res:
+                merged[topic] = word_dict[topic]
+
 def filter_text(str):
     return str.split()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    merged = {}
     dicts = []
     requestString = flask.request.data.decode("utf-8")
     requestDict = json.loads(requestString)
@@ -22,7 +36,9 @@ def home():
     word_list = list(itertools.chain.from_iterable(itertools.repeat(x, 100) for x in word_list))
     for word in word_list:
         get_topics(word, dicts)
-    print(dicts)
+
+    merge_topics(dicts, merged)
+    requestDict["topics"] = merged
     return json.dumps(requestDict)
 
 jsonLoc = backend.loadJson()
